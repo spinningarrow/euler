6c54c8f965d19a7c0877b7b48d88393fdac1ee63
Sahil Bajaj <sahil29@gmail.com>
2016-10-17 01:21:27 +0800

    Problem 2

--- euler.clj
+++ euler.clj
@@ -4,6 +4,10 @@
   {:pre [(not (zero? divisor))]}
   (zero? (mod num divisor)))
 
+(defn- fibonacci
+  ([] (fibonacci 0 1))
+  ([a b] (lazy-seq (cons a (fibonacci b (+ a b))))))
+
 ;; 1
 (defn multiples-of-3-and-5
   [limit]
@@ -11,3 +15,11 @@
                    (take limit (range)))))
 
 (multiples-of-3-and-5 1000)
+
+;; 2
+(defn even-fibonacci-numbers
+  [max]
+  (apply + (filter even?
+                   (take-while #(< % max) (fibonacci)))))
+
+(even-fibonacci-numbers 4000000)
